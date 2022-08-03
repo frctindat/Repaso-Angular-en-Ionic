b687c28536ce76da63d6fc3fbe8a1745dbf0fec1
frctindat <fcorrea@mindjob.com.ar>
2022-08-03 09:52:39 -0300

    Fix Clase 24

--- src/app/paginas/posteos/posteos-routing.module.ts
+++ src/app/paginas/posteos/posteos-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PosteosComponent } from './posteos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PosteosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
